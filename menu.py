e6647aab84461ad512a66d73d0cf5848504a7866
Maurice Beseng <128712700+mubeseng@users.noreply.github.com>
2023-07-12 22:57:09 +0100

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,11 @@
+#i ask a user to input his or her favourite food items
+
+item1 = input("Please order your first favourite food?: ")
+item2 = input("Please order your second favourite food item?: ")
+item3 = input("Please order your third faourite food item?: ")
+
+#i want the ouput to be displayed seperately
+print("Order confirmed! You have ordered:")
+print(item1)
+print(item2)
+print(item3)
